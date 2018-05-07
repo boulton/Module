e9816d32a32afa6ff3c6a94bdccdae81ada0321b
florian Geffry <f.geffry@gmail.com>
2018-05-07 11:02:01 +0200

    zebi

--- src/main/java/commons/main.java
+++ src/main/java/commons/main.java
@@ -8,7 +8,7 @@ public class MaClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated constructor stub
 		System.out.println(monPack.MyStringUtils.isNullOrEmpty("Test"));
-		
+		// le COMMENTAIRE		
 	}
 
 }
