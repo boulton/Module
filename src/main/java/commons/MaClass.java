00c5e9d2328f97471790a738af230924cae3683a
florian Geffry <f.geffry@gmail.com>
2018-05-07 10:05:50 +0200

    .\pom.xml

---
+++ src/main/java/commons/MaClass.java
@@ -0,0 +1,14 @@
+package commons;
+
+import monPack.*;
+
+public class MaClass {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated constructor stub
+		System.out.println(monPack.MyStringUtils.isNullOrEmpty("Test"));
+		
+	}
+
+}
