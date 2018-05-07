7e61fcfff46b4304e302ccaff651cf0834665eef
florian Geffry <f.geffry@gmail.com>
2018-05-07 11:33:02 +0200

    ntro de maVar

--- src/main/java/commons/main.java
+++ src/main/java/commons/main.java
@@ -4,7 +4,8 @@ import monPack.*;
 
 public class MaClass {
 	
-	
+	int mavar = 0;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated constructor stub
 		System.out.println(monPack.MyStringUtils.isNullOrEmpty("Test"));
