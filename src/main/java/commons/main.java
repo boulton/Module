6c89af89c360f6af55cf49114cba44b5ce9db9ae
florian Geffry <f.geffry@gmail.com>
2018-05-07 11:48:28 +0200

    mon dernier commit

--- src/main/java/commons/main.java
+++ src/main/java/commons/main.java
@@ -4,7 +4,10 @@ import monPack.*;
 
 public class MaClass {
 	
-	
+	int mavar = 0;
+	int var3 = 0;
+	int var2 = 1;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated constructor stub
 		System.out.println(monPack.MyStringUtils.isNullOrEmpty("Test"));
